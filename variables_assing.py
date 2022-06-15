437cc209587b46e6366965d65413d647731a815e
VENKATA RAGURAM KN <107139560+venkata2310@users.noreply.github.com>
2022-06-15 10:23:47 +0530

    Add files via upload

---
+++ variables_assing.py
@@ -0,0 +1,27 @@
+# "ways to assing variables"
+# # a=1
+# # b=2
+# # c=3
+# # print(a)
+# # print(b)
+# # print(c)
+
+# # a=1 ; b=2 ;c=3
+# # print(a,b,c)
+# # print(a) ; print(b) ; print(c)
+
+# a=c=5 ; b=4
+# print(a,b,c)
+# print(c,c,a)
+# print(a,c,b)
+
+# a=1
+# b=2
+# c=3
+# print(a)
+# print(b)
+# print(c)
+# print(d)
+
+# n=1,2,3,4
+# print(n,type(n))
